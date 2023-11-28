7acb82440d5c47c6ba2149aff2451c2d3c7d1cd1
MaharshiJ88 <136585286+MaharshiJ88@users.noreply.github.com>
2023-11-28 23:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function shortcut(s1, s2) {
+function shortcut(str1, str2) {
   // your code here
+	if (str1.length > 0 && str2.length > 0) {
+    // Use the first character of each string
+    return str1[0].toUpperCase() + str2[0].toLowerCase();
+  } else {
+    // If either of the strings is empty, return an empty string
+    return '';
+  }
 }
 
 // Do not change the code below.
